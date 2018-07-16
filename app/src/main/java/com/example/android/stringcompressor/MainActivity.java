9fd6fcac00a61ca367cb095ea20522ea54a38b8f
Denny Casabianca <casabiancadenny@gmail.com>
2018-07-16 08:58:42 +0200

    Fixed Starting and Ending with repeated chars cases

--- app/src/main/java/com/example/android/stringcompressor/MainActivity.java
+++ app/src/main/java/com/example/android/stringcompressor/MainActivity.java
@@ -1,19 +1,13 @@
 package com.example.android.stringcompressor;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
     private static TextView mResultTV;
@@ -36,30 +30,46 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public static String compressString(String input){
-        if(input.isEmpty()){
+    public static String compressString(String input) {
+        if (input.isEmpty()) {
             //check if the String is null (in case the check is done before this would serve as double check
-            return null;
+            return "no input detected";
         }
-        char[] inputCharArray =input.toCharArray();
-        ArrayList<Character> compressionHelper = new ArrayList<Character>();
+        final char[] inputCharArray = input.toCharArray();
+        final ArrayList<Character> compressionHelper = new ArrayList<Character>();
         int repetationCounter;
-        for (int i=0;i<inputCharArray.length;i=i+1+repetationCounter){
-            repetationCounter=0;
-            for(int j=0;inputCharArray[i]==inputCharArray[i+j];j++){
-                repetationCounter=j;
+        for (int i = 0; i < inputCharArray.length; i = i + 1 + repetationCounter) {
+            repetationCounter = 0;
+            if ((i + 1) != inputCharArray.length) {
+                for (int j = 0; inputCharArray[i] == inputCharArray[i + j]; j++) {
+                    repetationCounter = j;
+                    if ((j + 1) == inputCharArray.length) {
+                        //the string is a vector of only one char repeated
+                        break;
+                    }
+                    if ((j + i + 1) == inputCharArray.length) {
+                        //the string ends with a repeatedchar
+                        break;
+                    }
+                }
             }
-            compressionHelper.add(inputCharArray[i]);
-            if(repetationCounter==0){
-                compressionHelper.add('1');
-            }
-            else{
-                repetationCounter++;
-                compressionHelper.add(Integer.toString(repetationCounter).charAt(0));
-            }
-            }
-        return  compressionHelper.toString();
-        }
 
+            compressionHelper.add(inputCharArray[i]);
+            if (repetationCounter == 0) {
+                compressionHelper.add('1');
+            } else {
+                compressionHelper.add(Integer.toString(repetationCounter + 1).charAt(0));
+            }
+        }
+        if (compressionHelper.size() < inputCharArray.length) {
+            String listPolisher = new String();
+            for (int i = 0; i < compressionHelper.size(); i++) {
+                listPolisher = listPolisher + compressionHelper.get(i);
+            }
+            return listPolisher;
+        } else {
+            return input;
+        }
     }
+}
 
